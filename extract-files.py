f3e92c236fba00731ee5b3342f4896284836c5e4
Sean Du <do4suki@gmail.com>
2026-09-05 00:49:06 +0800

    clean 5
    Change-Id: Id80af8cfe6d34c999b0f88606585e11666e43f7a
    Signed-off-by: Sean Du <do4suki@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -229,8 +229,6 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace('/my_product', '/product'),
     'system_ext/bin/horae': blob_fixup()
         .replace_needed('libprotobuf-cpp-lite.so', 'libprotobuf-cpp-lite-21.7.so'),
-    'system_ext/lib64/vendor.qti.hardware.qccsyshal@1.2-halimpl.so': blob_fixup()
-        .replace_needed('libprotobuf-cpp-full.so', 'libprotobuf-cpp-full-21.7.so'),
     'vendor/etc/seccomp_policy/atfwd@2.0.policy': blob_fixup()
         .add_line_if_missing('lseek: 1'),
     'vendor/bin/init.kernel.post_boot-memory.sh': blob_fixup()
