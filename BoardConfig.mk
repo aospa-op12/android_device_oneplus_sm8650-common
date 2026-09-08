d36472aaa2e1de6d52a6bba71691ef804ea508af
Sean Du <do4suki@gmail.com>
2026-09-08 15:35:22 +0800

    Revert "enforcing"
    This reverts commit 6a2a630daa3752d1b36bbc694161d19bc2cf4fb1.
    
    Signed-off-by: Sean Du <do4suki@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -72,7 +72,8 @@ BOARD_BOOTCONFIG := \
     androidboot.memcg=1 \
     androidboot.serialconsole=0 \
     androidboot.usbcontroller=a600000.dwc3 \
-    androidboot.vendor.qspa=true
+    androidboot.vendor.qspa=true \
+    androidboot.selinux=permissive
 
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := sysctl.kernel.firmware_config.force_sysfs_fallback=1
