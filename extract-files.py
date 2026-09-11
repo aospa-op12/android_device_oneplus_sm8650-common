93ba25fd622f7ca152d8bee542f505368f2c3f15
Sean Du <do4suki@gmail.com>
2026-09-12 07:26:14 +0800

    dv
    Change-Id: I30cacd3a0e789a3da2c67dfcd011445e71752cac
    Signed-off-by: Sean Du <do4suki@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -373,6 +373,39 @@ blob_fixups: blob_fixups_user_type = {
         ),
     'vendor/etc/libnfc-nci.conf': blob_fixup()
         .regex_replace('NFC_DEBUG_ENABLED=1', 'NFC_DEBUG_ENABLED=0'),
+    'vendor/etc/media_codecs_pineapple.xml': blob_fixup()
+        #FIX HDR ENCODER
+        .regex_replace(
+            r'<!--\s*<MediaCodec name="c2\.qti\.dv\.encoder" type="video/dolby-vision">',
+            r'<MediaCodec name="c2.qti.dv.encoder" type="video/dolby-vision">'
+        )
+        .regex_replace(
+            r'(<Limit name="performance-point-7680x4320" value="30" />\s*)</MediaCodec>\s*-->',
+            r'\1    <Feature name="profile-and-level" value="256-8" />\n'
+            r'            <Feature name="profile-and-level" value="256-256" />\n'
+            r'            <Feature name="profile-and-level" value="256-1024" />\n'
+            r'        </MediaCodec>'
+        )
+        #FIX HDR DECODER
+        .regex_replace(
+            r'<!--\s*<MediaCodec name="c2\.qti\.dv\.decoder" type="video/dolby-vision" >',
+            r'<MediaCodec name="c2.qti.dv.decoder" type="video/dolby-vision" >'
+        )
+        .regex_replace(
+            r'(<Limit name="performance-point-8192x4320" value="48" />\s*)</MediaCodec>',
+            r'\1    <Feature name="profile-and-level" value="256-8" />\n'
+            r'            <Feature name="profile-and-level" value="256-256" />\n'
+            r'            <Feature name="profile-and-level" value="256-1024" />\n'
+            r'        </MediaCodec>'
+        )
+        #FIX HDR DECODER SECURE
+        .regex_replace(
+            r'(<Limit name="performance-point-4096x2304" value="120" />\s*)</MediaCodec>\s*-->',
+            r'\1    <Feature name="profile-and-level" value="256-8" />\n'
+            r'            <Feature name="profile-and-level" value="256-256" />\n'
+            r'            <Feature name="profile-and-level" value="256-1024" />\n'
+            r'        </MediaCodec>'
+        ),
     'vendor/etc/pwr/PowerFeatureConfig.xml': blob_fixup()
         .regex_replace(r'(<Name>GamePowerOptFeature</Name>\s*<Enable>)0(<\/Enable>)', r'\g<1>1\g<2>'),
     'vendor/etc/seccomp_policy/gnss@2.0-qsap-location.policy': blob_fixup()
